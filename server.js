5dc1fdd99b558a0dbdd833b2a3e40732af30dac8
Marina Bain <mjbain10@gmail.com>
2017-05-04 00:06:10 -0400

    ugh

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,6 +21,8 @@ app.use(express.static('./public'));
  
 require("./controllers/burger_controller.js")(app);
 
-app.listen(PORT, function() {
-	console.log("App listening on PORT " + PORT);
+app.set('port', process.env.PORT);
+
+app.listen(app.get('port'), () => {
+    console.log(`Express app listening on ${app.get('port')}`);
 });
