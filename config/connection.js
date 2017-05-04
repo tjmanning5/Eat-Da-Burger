93c2356d3eb27dc7f0649e9621450d4a28e8dd29
Marina Bain <mjbain10@gmail.com>
2017-05-03 23:24:29 -0400

    Attempting heroko deploy

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,23 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "nuclearwinter",
-	database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "nuclearwinter",
+// 	database: "burgers_db"
+// });
+
+var db = process.env.NODE_ENV === 'production' ?
+	process.env.JAWSDB_URL :
+	{
+		host: process.env.MYSQL_HOST,
+		port: process.env.MYSQL_PORT,
+		user: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PASSWORD,
+		database: process.env.MYSQL_DATABASE
+	};
+
+var connection = mysql.createConnection(db);
 
 connection.connect(function(err) {
 	if (err) {
