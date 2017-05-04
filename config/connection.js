104c741356afce0d3b4bebf3d892825cc35fec4c
Marina Bain <mjbain10@gmail.com>
2017-05-04 00:17:41 -0400

    ugh

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,19 @@ var mysql = require("mysql");
 // 	password: "nuclearwinter",
 // 	database: "burgers_db"
 // });
+var connection;
 
-var db = process.env.NODE_ENV === 'production'
-            ? process.env.JAWSDB_URL
-            : { host: process.env.MYSQL_HOST,
-                port: process.env.MYSQL_PORT,
-                user: process.env.MYSQL_USER,
-                password: process.env.MYSQL_PASSWORD,
-                database: process.env.MYSQL_DATABASE };
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'nuclearwinter',
+		database: 'burgers_db'
+	});
+};
 
-
-var connection = mysql.createConnection(db);
 
 connection.connect(function(err) {
 	if (err) {
