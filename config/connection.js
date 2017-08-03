b13ee7a2588b1a019cb5f0e4c78ee301c5afb42f
Marina Bain <mjbain10@gmail.com>
2017-08-03 18:08:53 -0400

    JawsDB config cleanup

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,5 @@
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-// 	host: "localhost",
-// 	user: "root",
-// 	password: "nuclearwinter",
-// 	database: "burgers_db"
-// });
 var connection;
 
 if (process.env.JAWSDB_URL) {
